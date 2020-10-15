f58a334e26a8434d529d37df501cd30af3a99f45
Michaelrod117 <michaelrod117@gmail.com>
2020-10-15 17:48:54 -0400

    about to add resume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,26 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { MyResumeComponent } from './my-resume/my-resume.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: '', component: ProductListComponent },     
+       { path: 'products/:productId', component: ProductDetailsComponent },{ path: '', component: ProductListComponent },
+
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    MyResumeComponent
   ],
   bootstrap: [ AppComponent ]
 })
